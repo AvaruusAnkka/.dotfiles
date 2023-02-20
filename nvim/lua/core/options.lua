cc6aa3ada3481e462a89940743ff7ea5df4741d6
Andy <pmduck@hotmail.com>
2023-02-20 14:44:32 +0200

    added tmux and nvim

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,13 @@
+local o = vim.opt
+
+o.nu = true
+o.rnu = true
+
+o.ts = 2
+o.sw = 2
+o.si = true
+o.scs = true
+
+o.so = 4
+
+-- o.tgc = true
---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,39 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+	use {
+		  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+			requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use 'neovim/nvim-lspconfig' -- configurations for nvim lsp
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+			
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
