85e09d6b05379144ede223fb9f87df329ed48c01
Andy <pmduck@hotmail.com>
2023-03-06 08:10:24 +0200

    reconfigured maps and comments

--- nvim/lua/core/maps.lua
+++ nvim/lua/core/maps.lua
@@ -1,7 +1,22 @@
-vim.g.mapleader = ' '
+local set = vim.keymap.set -- Set keymap
 
+vim.g.mapleader = ' ' -- Set leader key to space
+
+set('n', '<leader>r', ':luafile ~/.dotfiles/nvim/init.lua<CR>') -- Reload config
+
+-- Clear search highlighting
+set('n', '<leader><space>', ':noh<CR>')
+
+set('n', '<leader>p', ':Prettier<CR>') -- Prettier
+
+-- Tab managing
+set('n', '<Tab>', ':bnext<CR>')
+set('n', '<S-Tab>', ':bprevious<CR>')
+set('n', '<leader>d', ':bdelete<CR>')
+
+-- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+set('n', '<leader>ff', builtin.find_files, {})
+set('n', '<leader>fg', builtin.live_grep, {})
+set('n', '<leader>fb', builtin.buffers, {})
+set('n', '<leader>fh', builtin.help_tags, {})
