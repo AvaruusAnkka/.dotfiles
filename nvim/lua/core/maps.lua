71f92bcec1f24acf10b7abeee9815c03d735592f
Andy <pmduck@hotmail.com>
2023-03-09 15:30:58 +0200

    comment placement

--- nvim/lua/core/maps.lua
+++ nvim/lua/core/maps.lua
@@ -4,8 +4,7 @@ vim.g.mapleader = ' ' -- Set leader key to space
 
 set('n', '<leader>r', ':luafile ~/.dotfiles/nvim/init.lua<CR>') -- Reload config
 
--- Clear search highlighting
-set('n', '<leader><space>', ':noh<CR>')
+set('n', '<leader><space>', ':noh<CR>') -- Clear highlights
 
 set('n', '<leader>p', ':Prettier<CR>') -- Prettier
 
